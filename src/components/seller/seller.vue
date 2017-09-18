cc907ba8bce094c1727517f9a39412d600ad0465
Gray ge <ge.sf.chn@gmail.com>
2017-09-18 08:37:00 +0800

    favorite

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,6 +28,10 @@
             </div>
           </li>
         </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active': favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bullentin">
@@ -42,6 +46,26 @@
           </li>
         </ul>
       </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+              <li class="pic-item" v-for="(item, index) in seller.pics">
+                  <img :src="item" width="120" height="90">
+              </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="infos">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="(item, index) in seller.infos">
+            {{item}}
+          </li>
+        </ul>
+      </div>      
     </div>
   </div>
 </template>
@@ -49,6 +73,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import star from 'components/star/star';
+  import {saveToLocal, loadFromLocal} from 'common/js/store';
   import split from 'components/split/split';
 
   export default {
@@ -57,7 +82,26 @@
         type: Object
       }
     },
+    data() {
+      return {
+        favorite: (() => {
+          return loadFromLocal(this.seller.id, 'favorite', false);
+        })()
+      };
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? '已收藏' : '收藏';
+      }
+    },
     methods: {
+      toggleFavorite(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+        saveToLocal(this.seller.id, 'favorite', this.favorite);
+      },
       _initScroll() {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.seller, {
@@ -66,7 +110,25 @@
         } else {
           this.scroll.refresh();
         }
+      },
+    _initPics() {
+      if (this.seller.pics) {
+        let picWidth = 120;
+        let margin = 6;
+        let width = (picWidth + margin) * this.seller.pics.length - margin;
+        this.$refs.picList.style.width = width + 'px';
+        this.$nextTick(() => {
+          if (!this.picScroll) {
+            this.picScroll = new BScroll(this.$refs.picWrapper, {
+              scrollX: true,
+              eventPassthrough: 'vertical'
+            });
+          } else {
+            this.picScroll.refresh();
+          }
+        });
       }
+    }
     },
     mounted() {
       this.$nextTick(() => {
@@ -85,6 +147,7 @@
       'seller'() {
         this.$nextTick(() => {
           this._initScroll();
+          this._initPics();
         });
       }
     }
@@ -140,10 +203,27 @@
           .content
             line-height: 24px
             font-size: 10px
-            font-weight: 200
             color: rgb(7, 17, 27)
             .stress
               font-size: 24px
+      .favorite
+        position: absolute
+        width: 50px
+        right: 11px
+        top: 18px
+        text-align: center
+        .icon-favorite
+          display: block
+          margin-bottom: 4px
+          line-height: 24px
+          font-size: 24px
+          color: #d4d6d9
+          &.active
+            color: rgb(240, 20, 20)
+        .text
+          line-height: 10px
+          color: rgb(77, 85, 93)
+          font-size: 10px
     .bullentin
       padding: 18px 18px 0 18px
       .title
@@ -157,13 +237,14 @@
         .content
           line-height: 24px
           font-size: 12px
-          font-weight: 200
           color: rgb(240, 20, 20)
       .supports
         .support-item
           padding: 16px 12px
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 0
+          &:last-child
+            border-none()
           .icon 
             display: inline-block
             width: 16px
@@ -185,7 +266,41 @@
           .text
             line-height: 16px
             font-size: 12px
-            font-weight: 200
             color: rgb(7, 17, 27)
-
+    .pics
+      padding: 18px
+      .title
+        margin-bottom: 12px
+        line-height: 14px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .pic-wrapper
+        width: 100%
+        overflow: hidden
+        white-space: nowrap
+        .pic-list
+          font-size: 0
+          .pic-item
+            display: inline-block
+            margin-right: 6px
+            width: 120px
+            height: 90px
+            &:last-child
+              margin: 0
+    .infos
+      padding: 18px 18px 0 18px
+      color: rgb(7, 17, 27)
+      .title
+        padding-bottom: 12px
+        line-height: 14px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+        border-1px(rgba(7, 17, 27, 0.1))
+      .info-item
+        padding: 16px 12px
+        line-height: 16px
+        font-size: 12px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
 </style>
